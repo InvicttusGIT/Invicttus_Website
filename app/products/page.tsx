db5ce2f528e3318d631933d3ccec30f025d52fc7
M Shahzeb <shah.zeb@invicttus.com>
2025-07-01 18:45:39 +0500

    link updates

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -11,7 +11,7 @@ const apps = [
     image: "/fireplace.png",
     featured: false,
     tag: "Utility",
-    platform: ["FireTV", "Roku"],
+    platform: ["Roku", "FireTV"],
     device: ["Website"],
     link: "https://channelstore.roku.com/en-gb/details/519864a69c8bb82bafba4a6f5971f4f4:3b82cf6bc5bb38d94bd001a19dfb2584/fireplace-ambience-virtual-fire-place-for-tv",
   },
@@ -20,9 +20,9 @@ const apps = [
     image: "/Aquariumfree.jpg",
     featured: false,
     tag: "Featured",
-    platform: ["Roku", "FireTV"],
+    platform: ["Roku"],
     device: ["Website"],
-    link: "https://channelstore.roku.com/en-gb/details/c06aad6c62b7733825e71df47fcda2e3:cf08af00367f617bb8f5e0ea30e2f014/aquarium-free-relaxing-fish-and-coral-reef-videos-with-music-4k-tv",
+    link: "https://channelstore.roku.com/en-gb/details/b53c8b6b08e0017601a71983739ec6eb:e2dcaf40db84c6fec613205988bcf6fc/aquarium-free-relaxing-fish-and-coral-reef-videos-with-music-4k-tv",
   },
   {
     name: "USB Media Player",
@@ -31,7 +31,7 @@ const apps = [
     tag: "Utility",
     platform: ["Roku"],
     device: ["Website"],
-    link: "https://channelstore.roku.com/details/03e098489e45dc46200f70133df25863/usb-player-photos-and-videos",
+    link: "https://channelstore.roku.com/en-gb/details/c13f655b2e2d39a3916e4c6d70c804fa:3a0d5254dff6946bc7fa642f83bc9cbf/usb-media-file-explorer",
   },
   {
     name: "Guess The Word",
@@ -43,11 +43,11 @@ const apps = [
     link: "https://channelstore.roku.com/en-gb/details/2bd0b6ea73cb6d081aadc017c50f6d61:eed6f78387ae9b4e3241f0e07bd285f5/guess-the-word-word-search-by-pics-a-puzzle-and-brain-trivia-game-for-kids",
   },
   {
-    name: "WordScrapes",
+    name: "WordScapes",
     image: "/wordscrapes.png",
     featured: false,
     tag: "Utility",
-    platform: ["FireTV", "Roku"],
+    platform: ["Roku"],
     device: ["Website"],
     link: "https://channelstore.roku.com/en-gb/details/16742c526649c011964e4d4705c8c994/wordscapes-discover-crossword-brain-trivia-challenges-to-elevate-vocabulary-connect-and-match-letters-to-boost-your-iq-level-word-scapes",
   },
@@ -56,7 +56,7 @@ const apps = [
     image: "/word-search-puzzle.png",
     featured: false,
     tag: "Game",
-    platform: ["FireTV", "Roku"],
+    platform: ["Roku"],
     device: ["Website"],
     link: "https://channelstore.roku.com/en-ot/details/7ccb39d2b4485aef36e3fe60d6c52a59/word-search-game-find-words-adventure-trivia-to-enhance-your-vocabulary-skills-match-puzzle-and-elevate-your-brain-and-iq-level-for-kids-free",
   },
@@ -66,8 +66,8 @@ const apps = [
     featured: false,
     tag: "Utility",
     platform: ["FireTV"],
-    device: ["Website"],
-    link: "https://www.amazon.com/Alarm-Fire-TV-Tablets-Wallpapers/dp/B0CCRWTM9J/ref=sr_1_2?dib=eyJ2IjoiMSJ9.Ptv5GPB4yEBypwVUB6d8TzXglam379NBZ7DgwyaOU3c2fyN4TCi_WIGP1VXKZ_bmPZqEGn0xBn9i_SAIHK6jSg.-im9ZV9iAqz-F6q9XYKcRd1gjE00CNdfuol3F7qoDTA&dib_tag=se&qid=1737629880&refinements=p_4%3AApp+Oven&sr=8-2",
+    device: ["Website", "Tablets"],
+    link: "https://www.amazon.com/Alarm-Fire-TV-Tablets-Wallpapers/dp/B0CCRWTM9J/ref=sr_1_4?dib=eyJ2IjoiMSJ9.hhkf8MmFe1ntYXL7EiqJeQT-QekYQ_Aef0BJ0clhr8ZlMTPgkV5_p2kdGzTAIl2MFBBKdDdDnBnhbu12vB3eE4yR67_ggBW8UVCH0Ss5haS0atDO-_IWpIGNNHgWXuPtG2SSQVtrqUCme9N-8Dt6NwsbUnNo8TX45lIoTI6tuhffkXqrg4mXeiftJchrDIB9.GOpCUjfuF6RWcsakWUWnugtEbA7nOPb2XX4YyGIDKus&dib_tag=se&qid=1751375767&refinements=p_4%3AApp+Oven&s=mobile-apps&search-type=ss&sr=1-4",
   },
   {
     name: "Clockfaces",
@@ -75,8 +75,8 @@ const apps = [
     featured: false,
     tag: "Utility",
     platform: ["FireTV"],
-    device: ["Website"],
-    link: "https://www.amazon.com/Clock-Faces-Fire-Tablets-featuring/dp/B0CPPWX8R2/ref=sr_1_4?dib=eyJ2IjoiMSJ9.Ptv5GPB4yEBypwVUB6d8TzXglam379NBZ7DgwyaOU3c2fyN4TCi_WIGP1VXKZ_bmPZqEGn0xBn9i_SAIHK6jSg.-im9ZV9iAqz-F6q9XYKcRd1gjE00CNdfuol3F7qoDTA&dib_tag=se&qid=1737629880&refinements=p_4%3AApp+Oven&sr=8-4",
+    device: ["Website", "Tablets"],
+    link: "https://www.amazon.com/Clock-Faces-Fire-Tablets-featuring/dp/B0CPPWX8R2/ref=sr_1_6?dib=eyJ2IjoiMSJ9.hhkf8MmFe1ntYXL7EiqJeQT-QekYQ_Aef0BJ0clhr8ZlMTPgkV5_p2kdGzTAIl2MFBBKdDdDnBnhbu12vB3eE4yR67_ggBW8UVCH0Ss5haS0atDO-_IWpIGNNHgWXuPtG2SSQVtrqUCme9N-8Dt6NwsbUnNo8TX45lIoTI6tuhffkXqrg4mXeiftJchrDIB9.GOpCUjfuF6RWcsakWUWnugtEbA7nOPb2XX4YyGIDKus&dib_tag=se&qid=1751376140&refinements=p_4%3AApp+Oven&s=mobile-apps&search-type=ss&sr=1-6",
   },
 ];
 const platformIcons: Record<string, string> = {
@@ -145,34 +145,41 @@ export default function ProductsPage() {
                     high-octane games, and next-gen VR/MR experiences.
                   </p>
                 </div>
-                <div className={styles.productSectionRight}>
-                  <div className={styles.ProductSectionLeft}>
-                    <h4 className={styles.ProductSectionAppName}>
-                      Guess The Word
-                    </h4>
-                    <Image
-                      src="/9ad56e4d22b398af9bd855825f4c44222d3b7971.png"
-                      alt="Guess The Word"
-                      width={357}
-                      height={293}
-                      className={styles.ProductSectionImage}
-                    />
-                  </div>
-                  <div className={styles.ProductSectionRightContent}>
-                    <div className={styles.ProductSectionStats}>
-                      <span className={styles.ProductSectionStatBox}>
-                        <p>
-                          <strong>4.9 </strong> Ratings
-                        </p>
-                      </span>
-                      <span className={styles.ProductSectionStatBox}>
-                        <p>
-                          <strong> 1M </strong> Userbase
-                        </p>
-                      </span>
+                <a
+                  href="https://channelstore.roku.com/en-gb/details/2bd0b6ea73cb6d081aadc017c50f6d61:eed6f78387ae9b4e3241f0e07bd285f5/guess-the-word-word-search-by-pics-a-puzzle-and-brain-trivia-game-for-kids"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ textDecoration: "none" }}
+                >
+                  <div className={styles.productSectionRight}>
+                    <div className={styles.ProductSectionLeft}>
+                      <h4 className={styles.ProductSectionAppName}>
+                        Guess The Word
+                      </h4>
+                      <Image
+                        src="/9ad56e4d22b398af9bd855825f4c44222d3b7971.png"
+                        alt="Guess The Word"
+                        width={357}
+                        height={293}
+                        className={styles.ProductSectionImage}
+                      />
+                    </div>
+                    <div className={styles.ProductSectionRightContent}>
+                      <div className={styles.ProductSectionStats}>
+                        <span className={styles.ProductSectionStatBox}>
+                          <p>
+                            <strong>4.9</strong> Ratings
+                          </p>
+                        </span>
+                        <span className={styles.ProductSectionStatBox}>
+                          <p>
+                            <strong>1M</strong> Userbase
+                          </p>
+                        </span>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </a>
               </div>
               <div className={styles.PrductsSectionFilter}>
                 <button
